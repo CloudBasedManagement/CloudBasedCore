cf794df19a5f41ed17c9df36a61f23e9551481e3
firatyenidunya <firatyenidunya@gmail.com>
2017-04-16 01:13:30 +0300

    google drive authorization

---
+++ CBC.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from GoogleDrive import *
+class CBC:
+
+    def GoogleDrive(self, client_id, client_secret, redirect_uri):
+        return GoogleDrive(client_id,client_secret, redirect_uri)
